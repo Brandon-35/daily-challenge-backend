2567da55250a1e680a9073befc00a1e5e0b99e1f
BrandonTa <brandonta1035@gmail.com>
2025-02-18 12:38:41 +0700

    fix: Add socket.io dependency
    - Added socket.io package
    - Updated socket service configuration
    - Added CORS settings for WebSocket
    - Fixed server initialization

--- src/services/socket_service.js
+++ src/services/socket_service.js
@@ -4,7 +4,12 @@ let io;
 
 module.exports = {
     init: (server) => {
-        io = socketIO(server);
+        io = socketIO(server, {
+            cors: {
+                origin: process.env.CORS_ORIGIN || '*',
+                methods: ['GET', 'POST']
+            }
+        });
         
         io.on('connection', (socket) => {
             socket.on('join', (user_id) => {
